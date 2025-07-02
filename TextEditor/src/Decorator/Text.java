f7a2dd6c06afba2ff359f234207fee90fe1ffe10
ChemieAi <kzlyburak@gmail.com>
2025-07-02 23:18:49 +0300

    Initial commit

---
+++ TextEditor/src/Decorator/Text.java
@@ -0,0 +1,8 @@
+
+package Decorator;
+
+public interface Text {
+    void decorate();
+}
+// Component interface
+// İmplemente edilecek methodun bulunduğu class'dır
---
+++ TextEditor/src/Factory/Fac_Theme_Interface.java
@@ -0,0 +1,8 @@
+package Factory;
+
+
+
+public interface Fac_Theme_Interface {
+    
+    public void changeTheme();
+}
